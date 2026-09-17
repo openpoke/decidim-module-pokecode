331b41e38c87a8314fef3ea5495056ff27da4438
David Igón <david@pokecode.net>
2026-09-17 16:08:48 +0200

    set version to 0.4.0

--- lib/decidim/pokecode/version.rb
+++ lib/decidim/pokecode/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Pokecode
-    VERSION = "0.3.0"
+    VERSION = "0.4.0"
     COMPAT_DECIDIM_VERSION = [">= 0.32", "< 0.33"].freeze
   end
 end
