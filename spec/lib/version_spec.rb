ec8fcc4456cab05e0e20b7ba6c4b30eee315d737
David Igón <david@pokecode.net>
2026-09-17 16:26:40 +0200

    update version spec

--- spec/lib/version_spec.rb
+++ spec/lib/version_spec.rb
@@ -7,7 +7,7 @@ module Decidim
     subject { described_class }
 
     it "has version" do
-      expect(subject::VERSION).to eq("0.3.0")
+      expect(subject::VERSION).to eq("0.4.0")
       expect(subject::COMPAT_DECIDIM_VERSION).to eq([">= 0.32", "< 0.33"])
     end
   end
