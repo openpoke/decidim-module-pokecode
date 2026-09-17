03dbaf70208af0f412d346b8740d0a4b99f25c53
David Igón <david@pokecode.net>
2026-09-17 16:27:04 +0200

    change sidekiq spec to use I18n.locale for localization of endpoint

--- spec/system/admin_sidekiq_spec.rb
+++ spec/system/admin_sidekiq_spec.rb
@@ -35,7 +35,7 @@ describe "Admin" do
 
     it "denies access to /sidekiq for unauthenticated users" do
       visit "/sidekiq"
-      expect(page).to have_current_path("/en/users/sign_in")
+      expect(page).to have_current_path("/#{I18n.locale}/users/sign_in")
     end
   else
     it "denies access to /sidekiq for all users" do
